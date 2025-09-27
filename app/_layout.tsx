29bb3d0da1f713beee345c49158a3d811b829c05
shubham sharma <shubhamtech004@gmail.com>
2025-09-28 03:22:29 +0530

    stack navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerTitleAlign: "center", // 👈 centers the header title
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+    </Stack>
+  );
 }
