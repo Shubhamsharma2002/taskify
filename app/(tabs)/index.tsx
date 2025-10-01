c093622ec8575eee021e0a1bf8c1ecac99ff8df9
shubham sharma <shubhamtech004@gmail.com>
2025-10-01 19:09:08 +0530

    usetheme for switich the dark and light mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const {toggleDarkMode} = useTheme();
   return (
     <View
       style={styles.container}
     >
       <Text>Edit app/index.tsx to edit this screen oky we will.</Text>
-      
+     
+      <TouchableOpacity>
+        <Text onPress={toggleDarkMode}>swith to dark</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
+import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
+    <ThemeProvider>
     <Stack
       screenOptions={{
       
         headerShown:false 
       }}
     >
-      <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
+      <Stack.Screen name="(tabs)" />
       
     </Stack>
+    </ThemeProvider>
   );
 }
