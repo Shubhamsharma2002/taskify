92525af2312595d709bc22b7aaa32cffb879ff1e
shubham sharma <shubhamtech004@gmail.com>
2025-10-05 23:07:35 +0530

    testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,11 @@ import useTheme from "@/hooks/useTheme";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme();
+  const { toggleDarkMode } = useTheme();
   return (
-    <View
-      style={styles.container}
-    >
-
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen oky we will.</Text>
-     
-     
+
       <TouchableOpacity>
         <Text onPress={toggleDarkMode}>swith to dark</Text>
       </TouchableOpacity>
@@ -18,13 +14,11 @@ export default function Index() {
   );
 }
 
-
-
 const styles = StyleSheet.create({
-  container:{
-     flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor:"lightgreen"
-  }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgreen",
+  },
+});
